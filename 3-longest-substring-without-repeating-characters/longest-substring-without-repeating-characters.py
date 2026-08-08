f6dd8c5ffc4a1c88acb67341d2ce4d5373fc14c6
parnapallichandrakanth <parnapallichandrakanth45@gmail.com>
2026-08-08 10:01:49 +0530

    Time: 293 ms (7.46%) | Memory: 19.8 MB (13.39%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -5,9 +5,8 @@ class Solution:
         max_length=0
         for right in range(len(s)):
             d[s[right]]=d.get(s[right],0)+1
-            if d[s[right]]>1:
-                while d[s[right]]>1:
-                    d[s[left]]-=1
-                    left+=1
+            while d[s[right]]>1:
+                d[s[left]]-=1
+                left+=1
             max_length=max(max_length,right-left+1)
         return max_length         
