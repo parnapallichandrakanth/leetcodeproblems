cefc87d9a61a7c4e42747b26cc3606f5cc10afc3
parnapallichandrakanth <parnapallichandrakanth45@gmail.com>
2026-08-10 10:37:33 +0530

    Time: 11 ms (52.05%) | Memory: 20.5 MB (51.74%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -4,19 +4,15 @@ def is_v(k):
 class Solution:
     def reverseVowels(self, s: str) -> str:
         s=list(s)
-        i=0
-        j=len(s)-1
-        while i<j:
-            if is_v(s[i]) and is_v(s[j]):
-                s[i],s[j]=s[j],s[i]
-                i+=1
-                j-=1
-            elif is_v(s[i]):
-                j-=1
-            elif is_v(s[j]):
-                i+=1
-            else:
-                i+=1
-                j-=1
+        left=0
+        right=len(s)-1
+        while left<right:
+            while not is_v(s[left]) and left<right:
+                left+=1
+            while not is_v(s[right]) and left<right:
+                right-=1
+            s[left],s[right]=s[right],s[left]
+            left+=1
+            right-=1
         return ''.join(s)
         
