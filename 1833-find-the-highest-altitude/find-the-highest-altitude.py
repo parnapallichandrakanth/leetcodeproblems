2d74d2268f5a019bf566aa66c9dffca68e77a348
parnapallichandrakanth <parnapallichandrakanth45@gmail.com>
2026-08-12 11:49:31 +0530

    Time: 2 ms (7.56%) | Memory: 19.3 MB (49.65%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.py
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.py
@@ -1,5 +1,5 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
-        altitudes=itertools.accumulate(gain,initial=0)
+        altitudes=list(itertools.accumulate(gain,initial=0))
         return max(altitudes)
         
