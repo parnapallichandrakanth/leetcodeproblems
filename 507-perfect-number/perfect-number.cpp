e74eda7a5f2513e00ae2168edff7babfcd048d6b
parnapallichandrakanth <parnapallichandrakanth45@gmail.com>
2026-09-08 10:47:04 +0530

    Time: 1067 ms (33.99%) | Memory: 7.8 MB (55.02%) - LeetSync

---
+++ 507-perfect-number/perfect-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int s=0;
+        for(int i=1;i<=num/2;i++){
+            if(num%i==0){
+                s+=i;
+            }
+        }
+        if(s==num){
+            return 1;
+        }
+        return 0;
+
+    }
+};
