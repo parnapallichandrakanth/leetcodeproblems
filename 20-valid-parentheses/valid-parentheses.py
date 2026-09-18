9b993c83293663aca540a221457e50e0f0fa8e36
parnapallichandrakanth <parnapallichandrakanth45@gmail.com>
2026-09-18 11:20:37 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (63.83%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        st=[]
+        
+        for ch in s:
+            if ch in "{[(":
+                st.append(ch)
+            else:
+                if not st:
+                    return False
+                elif len(st)>=1:
+                    if ch=="}" and st[-1]=="{":
+                        st.pop()
+                    elif ch=="]" and st[-1]=="[":
+                        st.pop()
+                    elif ch==")" and st[-1]=="(":
+                        st.pop()
+                    else:
+                        return False
+                    
+        return not st
